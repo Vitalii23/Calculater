120d80ad129b37bb356d482c6419df6de8c112cc
Vitalii <danilov.vit.an@gmai.com>
2019-11-15 17:34:49 +0700

    prototype calculater

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,120 +3,77 @@ package Main;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner line = new Scanner(System.in);
+    private static Scanner line = new Scanner(System.in);
 
-    String x, y, z, operation;
+    private String x, y, z, operation;
+    private double result;
 
-    public void calculator() {
+    private double pemdas(String x, String y){
+        double a = Double.parseDouble(x);
+        double b = Double.parseDouble(y);
+        switch (operation) {
+            case "+":
+                return result = a + b;
+            case "-":
+                return result = a - b;
+            case "*":
+                return result = a * b;
+            case "/":
+            default:
+                if (b == 0) {
+                    System.out.println("You cannot divide by zero");
+                    break;
+                } else {
+                    return result = a / b;
+                }
+        }
+        return result;
+    }
+
+    private String calculator() {
+        System.out.print("Enter x >> ");
+        x = line.nextLine();
         while (true) {
-            System.out.print("Enter x >> ");
-            x = line.nextLine();
             System.out.print("Enter operation >> ");
             operation = line.nextLine();
             System.out.print("Enter y >> ");
             y = line.nextLine();
-            System.out.print("Enter action ('C', '=') >> ");
+            System.out.print("Enter symbol ('C', '=') >> ");
             z = line.nextLine();
             if ((z.charAt(0) >= '0' && z.charAt(0) <= '9') || z.charAt(0) == '.') {
+
                 if (!operation.equals("")) {
                     y = y + z;
                 } else {
                     x = x + z;
                 }
                 System.out.println(x + operation + y);
+
             } else if (z.charAt(0) == 'C') {
                 x = operation = y = "";
                 System.out.println(x + operation + y);
+                return x = calculator();
+
             } else if (z.charAt(0) == '=') {
-                double result;
-                if (operation.equals("+")) {
-                    result = (Double.parseDouble(x) + Double.parseDouble(y));
-                } else if (operation.equals("-")) {
-                    result = (Double.parseDouble(x) - Double.parseDouble(y));
-                } else if (operation.equals("/")) {
-                    result = (Double.parseDouble(x) / Double.parseDouble(y));
-                } else {
-                    result = (Double.parseDouble(x) * Double.parseDouble(y));
-                }
+                pemdas(x, y);
                 System.out.println(x + " " + operation + " " + y + " = " + result);
                 x = Double.toString(result);
                 operation = y = "";
+
             } else {
+
                 if (operation.equals("") || y.equals("")) {
                     operation = z;
+
                 } else {
-                    double result;
-                    if (operation.equals("+")) {
-                        result = (Double.parseDouble(x) + Double.parseDouble(y));
-                    } else if (operation.equals("-")) {
-                        result = (Double.parseDouble(x) - Double.parseDouble(y));
-                    } else if (operation.equals("/")) {
-                        result = (Double.parseDouble(x) / Double.parseDouble(y));
-                    } else {
-                        result = (Double.parseDouble(x) * Double.parseDouble(y));
-                    }
-
+                    pemdas(x, y);
                     x = Double.toString(result);
-
                     operation = z;
-
                     y = "";
                 }
                 System.out.println(x + " " + operation + " " + y);
             }
         }
-
-
-
-        /*if (operation.equals("+")) {
-                result = x + y;
-            } else if (operation.equals("-")) {
-                result = x - y;
-            } else if (operation.equals("*")) {
-                result = x * y;
-            } else if (operation.equals("/")) {
-                result = x / y;
-            } else {
-                System.out.print("Operator not recognized");
-            }
-            System.out.print("First variable equal = " + result + "\r\n");
-            System.out.print("Enter y >> ");
-            result += y;
-            System.out.println(result);*/
-
-
-        /*switch (operation) {
-            case "+":
-                result = x + y;
-                System.out.println(x + " + " + y + " = " + result);
-                while (result != 0) {
-                    System.out.print("First variable equal = " +result + "\r\n");
-                    System.out.print("Enter z >> ");
-                    z = line.nextDouble();
-                    result += z;
-                    System.out.println(result);
-                }
-                break;
-            case "-":
-                result = x - y;
-                System.out.println(x + " - " + y + " = " + result);
-                break;
-            case "*":
-                result = x * y;
-                System.out.println(x + " * " + y + " = " + result);
-                break;
-            case "/":
-                if (y == 0) {
-                    System.out.println("You cannot divide by zero");
-                    break;
-                } else {
-                    result = x / y;
-                    System.out.println(x + " / " + y + " = " + result);
-                    break;
-                }
-            default:
-                System.out.println("No operation");
-        }*/
     }
 
 
@@ -131,5 +88,4 @@ public class Main {
             main.calculator();
         }
     }
-
 }

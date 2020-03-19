5d0f6d55f7ab8f7e684b2846d5b086a3646fe243
Vitalii <danilov.vit.an@gmai.com>
2020-03-19 21:47:23 +0700

    prototype calculater

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,112 +1,32 @@
 package Main;
 
+import javax.script.ScriptException;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner line = new Scanner(System.in);
 
-    private String x, y, z, operation;
-    private double result;
-
-    private void operation(String x, String y){
-        double a = Double.parseDouble(x);
-        double b = Double.parseDouble(y);
-        switch (operation) {
-            case "+":
-                result = a + b;
-                System.out.println(x + " " + operation + " " + y + " = " + result);
-                return;
-            case "-":
-                result = a - b;
-                System.out.println(x + " " + operation + " " + y + " = " + result);
-                return;
-            case "*":
-                result = a * b;
-                System.out.println(x + " " + operation + " " + y + " = " + result);
-                return;
-            case "/":
-            default:
-                if (b == 0) {
-                    System.out.println("You cannot divide by zero");
-                    break;
-                } else {
-                    result = a / b;
-                    System.out.println(x + " " + operation + " " + y + " = " + result);
-                }
-        }
-    }
-
-    private String calculator() {
-        System.out.print("Enter x >> ");
-        x = line.nextLine();
-        if (x.matches("[0-9]+")){
-            System.out.println("Well done");
-        } else {
-            System.out.println("Error enter number. Let's try again");
-            return x = calculator();
-        }
-
-        while (true) {
-            System.out.print("Enter operation >> ");
-            operation = line.nextLine();
-
-            System.out.print("Enter y >> ");
-            y = line.nextLine();
-            if (y.matches("[0-9]+")){
-                System.out.println("Well done");
-            } else {
-                System.out.println("Error enter number. Let's try again");
-                return y = calculator();
-            }
-
-            System.out.print("Enter symbol ('C', '=') >> ");
-            z = line.nextLine();
-
-            if ((z.charAt(0) >= '0' && z.charAt(0) <= '9') || z.charAt(0) == '.') {
-
-                if (!operation.equals("")) {
-                    y = y + z;
-                } else {
-                    x = x + z;
-                }
-                System.out.println(x + operation + y);
-
-            } else if (z.charAt(0) == 'C') {
-                x = operation = y = "";
-                System.out.println(x + operation + y);
-                return x = calculator();
-
-            } else if (z.charAt(0) == '=') {
-                operation(x, y);
-                x = Double.toString(result);
-                operation = y = "";
-
-            } else {
-
-                if (operation.equals("") || y.equals("")) {
-                    operation = z;
-
-                } else {
-                    operation(x, y);
-                    x = Double.toString(result);
-                    operation = z;
-                    y = "";
-                }
-                System.out.println(x + " " + operation + " " + y);
-            }
-        }
-    }
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScriptException {
         if(args.length < 1) {
             System.out.println("Welcome applicatien calculater\r\n" +
-                    "Enter: <-start>");
+                    "Enter: <-arab> or <-roman>");
             return;
         }
-        Main main = new Main();
-        if (args[0].equals("-start")) {
-            main.calculator();
+
+        if (args[0].equals("-arab")) {
+            ArabNumber an = new ArabNumber();
+            Scanner line = new Scanner(System.in);
+            System.out.println("Welcome! Enter number");
+            String text = line.nextLine();
+            System.out.println("= " + an.arabCalc(text));
+        }
+
+        if (args[0].equals("-roman")) {
+            RomanNumber rn = new RomanNumber();
+            Scanner line = new Scanner(System.in);
+            System.out.println("Welcome! Enter number");
+            String text = line.nextLine();
+            int result = rn.calculator(text);
+            System.out.println("= " + rn.decimalRoman(result));
         }
     }
 }
